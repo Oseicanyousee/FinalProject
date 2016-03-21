9cee9f64212bae20f532b41af77c61204cc16581
Tom <tomalexanderphoto@gmail.com>
2016-03-21 15:27:19 -0400

    MODAL STUFF

--- public/js/directive/directive.js
+++ public/js/directive/directive.js
@@ -1,51 +1,45 @@
 angular.module('tourApp')
-	.directive('selfieMap', function(){
-		return {
-			restrict: "E",
-			templateUrl: "public/js/directive/myQuest.html",
-			replace: true,
-			link: function(scope, element, attrs){
-				element.css(
-					{
-						display: 'inline'
-					}
-				);
-			}
-		};
-	});
-// angular.module('tourApp')
-// 	.directive('selfieMap', function(){
-// 		return {
-// 			restrict: "E",
-// 			templateUrl: "public/js/directive/myQuest.html",
-// 			replace: true,
-// 			link: function(scope, element, attrs){
-// 				element.css(
-// 					{
-// 						display: 'inline'
-// 					}
-// 				);
-// 			}
-// 		};
-// 	});
+	.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
 
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
 
-// angular.module('tourApp')
-// 	.directive('selfieMap', function(){
-// 		return {
-// 			restrict: "E",
-// 			templateUrl: "public/js/directive/myQuest.html",
-// 			// template:'<h1>hi mom</h1>',
-// 			replace: true,
-// 			link: function(scope, element, attrs){
-// 				element.css(
-// 					{
-// 						display: 'inline'
-// 					}
-// 				);
-// 			}
-// 		};
-// 	});
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
 
 
 angular.module('tourApp')
