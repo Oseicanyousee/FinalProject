6ed59c11f7fef6300b159f623772a200b7c829e4
Tom <tomalexanderphoto@gmail.com>
2016-03-24 13:06:52 -0400

    Photo examples page good to goish probs 90% good

--- public/js/controllers/photoControl.js
+++ public/js/controllers/photoControl.js
@@ -1,17 +1,18 @@
 angular.module('tourApp')
 	.controller("photoControl", ["$scope", "Lightbox", function($scope, Lightbox){
-			$scope.images = [
-    {
-      'url': './public/images/darcy.jpg',
-      'thumbUrl': './public/images/darcyThumb.jpg',
-      'caption': 'Selfie Example'
-    },
-    {
-      'url': './public/images/brodrick.jpg',
-      'thumbUrl': './public/images/brodrickThumb.jpg'
-    }];
-    $scope.openLightboxModal = function (index) {
-    Lightbox.openModal($scope.images, index);
-  };
+		// 	$scope.images = [
+  //   {
+  //     'url': './public/images/darcy.jpg',
+  //     'thumbUrl': './public/images/darcyThumb.jpg',
+  //     'caption': 'Selfie Example'
+  //   },
+  //   {
+  //     'url': './public/images/brodrick.jpg',
+  //     'thumbUrl': './public/images/brodrickThumb.jpg',
+  //     'caption': 'weather: cloudy'
+  //   }];
+  //   $scope.openLightboxModal = function (index) {
+  //   Lightbox.openModal($scope.images, index);
+  // };
 	}]);
 
